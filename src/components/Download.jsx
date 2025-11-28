4faa540deb4861b6a8b48d32acc6223a5b17ed39
DiegoSalm522 <diegosalm522@gmail.com>
2025-11-28 11:29:08 -0600

    Some UI changes

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,7 +3,7 @@ const Download = () => {
     <a 
       href="/assets/files/DiegoGSA_Resume.pdf"
       download="DiegoGSA_Resume.pdf"
-      className="relative px-3 py-4 text-sm text-center rounded-full font-extralight bg-black lg:w-[15rem] cursor-pointer overflow-hidden inline-block no-underline w-auto"
+      className="relative px-3 py-4 text-sm text-center rounded-full font-extralight bg-black lg:w-[15rem] cursor-pointer overflow-hidden inline-block no-underline w-auto hover-animation"
     >
       <p className="flex items-center headtext justify-center gap-2 text-white">
         Download my Resume
